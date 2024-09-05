6ea1de397080bebee7df0828fbf629688cbe18bf
Mustafa53 Haider <mustafahaider523@gmail.com>
2024-09-05 06:15:19 +0000

    3rd video till nested loops

---
+++ 3video.cpp
@@ -0,0 +1,65 @@
+// #include <iostream>
+// #include <vector>
+// #include <string>
+// #include <algorithm>
+// using namespace std;
+
+// // Main function
+// int main() {
+
+    
+//     for(int i = 0; i<=2; i++){
+//         cout<<"Hello world"<<endl;
+//     }
+    
+//     return 0;
+// }
+
+#include<iostream>
+using namespace std;
+
+
+
+ 
+int main()
+{
+
+    
+    int sum = 0;
+    int n;
+
+    cout << "Enter value n : " << endl;
+    cin >> n;
+
+    
+    
+    
+    
+    
+
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     sum = sum + i;
+    // }
+
+    // cout << sum << endl;
+    
+    
+    for (int i = 0; i < 5 ; i++)
+    {
+        cout << "************" << endl;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+ 
+ 
+    return 0;
+} 
