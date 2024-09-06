7b7b580a611fa2ead0e4ff418ee21474184f381c
Mustafa53 Haider <mustafahaider523@gmail.com>
2024-09-06 05:57:23 +0000

    Q3

--- patterns.cpp
+++ patterns.cpp
@@ -4,36 +4,54 @@ using namespace std;
 int main()
 {
  // Q1
+    // int n;
+
+    // cout << "Enter value: " << endl;
+    // cin >> n;
+
+    // for (int i = 1; i <= n; i++) // for lines
+    // {
+    //     for (int j = 1; j <= n; j++) //for print
+    // {
+    //     cout << j;
+    // }
+    // cout  << endl;
+    // }
+    
+// Q2
+    
+    //  int n;
+
+    // cout << "Enter value: " << endl;
+    // cin >> n;
+
+    // for (int i = 1; i <= n; i++) // for lines
+    // {
+    //     for (int j = 1; j <= n; j++) //for print
+    // {
+    //     cout << "*" << " ";
+    // }
+    // cout  << endl;
+    // }  
+    
+//Q3
+
     int n;
 
     cout << "Enter value: " << endl;
     cin >> n;
 
-    for (int i = 1; i <= n; i++) // for lines
-    {
-        for (int j = 1; j <= n; j++) //for print
-    {
-        cout << j;
-    }
-    cout  << endl;
-    }
-    
-// Q2
-    
-     int n;
-
-    cout << "Enter value: " << endl;
-    cin >> n;
+    char c
+    cout << "Enter chracter: " << endl;
+    cin >> c;
 
     for (int i = 1; i <= n; i++) // for lines
     {
-        for (int j = 1; j <= n; j++) //for print
+        for (char j = 'A'; j <= c; j++) //for print
     {
-        cout << "*" << " ";
+        cout << j << " ";
     }
     cout  << endl;
-    }  
-    
- 
+    }
     return 0;
 }
