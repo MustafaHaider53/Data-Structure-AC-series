4ec0a9c1299842ae4c4a110578ef4ee5650f7f30
Mustafa53 Haider <mustafahaider523@gmail.com>
2024-09-06 07:43:28 +0000

    till Q10

--- patterns.cpp
+++ patterns.cpp
@@ -36,21 +36,177 @@ int main()
     
 //Q3
 
-    int n;
-    cout << "Enter value: " << endl;
-    cin >> n;
+    // int n;
+    // cout << "Enter value: " << endl;
+    // cin >> n;
 
     // char c;
     // cout << "Enter chracter: " << endl;
     // cin >> c;
 
-    for (int i = 1; i <= n; i++) // for lines
+    // for (int i = 1; i <= n; i++) // for lines
+    // {
+    //     for (char j = 'A'; j <= c; j++) //for print
+    // {
+    //     cout << j << " ";
+    // }
+    // cout  << endl;
+    // }
+
+// Q4
+
+    
+    // int n;
+
+    // cout << "Enter value: " << endl;
+    // cin >> n;
+
+    // int var = 0;
+
+    // for (int i = 1; i <= n; i++) // for lines
+    // {
+
+    //     for (int j = 1; j <= n; j++) //for print
+    // {
+    //     var++;
+    //     cout << var << " ";
+        
+    // }
+    // cout  << endl;
+    // }
+    
+ // Q5
+
+    // int n;
+
+    // cout << "Enter value: " << endl;
+    // cin >> n;
+
+    // char var = 64 ;
+
+    // for (int i = 1; i <= n; i++) // for lines
+    // {
+
+    //     for (int j = 1; j <= n; j++) //for print
+    // {
+    //     var++;
+    //     cout << var << " ";
+        
+    // }
+    // cout  << endl;
+    // }
+
+// Q6
+
+    // int n;
+
+    // cout << "Enter value: " << endl;
+    // cin >> n;
+
+    
+
+    // for (int i = 1; i <= n; i++) // for lines
+    // {
+
+    //     for (int j = 1; j <= i; j++) //for print
+    // {
+        
+    //     cout << "*" << " ";
+        
+    // }
+    // cout  << endl;
+    // }
+
+
+// Q7
+
+    // int n;
+
+    // cout << "Enter value: " << endl;
+    // cin >> n;
+
+    
+
+    // for (int i = 1; i <= n; i++) // for lines
+    // {
+
+    //     for (int j = 1; j <= i; j++) //for print
+    // {
+        
+    //     cout << i << " ";
+        
+    // }
+    // cout  << endl;
+    // }
+
+// Q8
+
+    // int n;
+
+    // cout << "Enter value: " << endl;
+    // cin >> n;
+
+    // char c = 64;    
+
+    // for (int i = 1; i <= n; i++) // for lines
+    // {
+
+    //     c++;
+
+    //     for (int j = 1; j <= i; j++) //for print
+    // {
+        
+    //     cout << c << " ";
+        
+    // }
+    // cout  << endl;
+    // }
+
+// Q9
+
+    // int n;
+
+    // cout << "Enter value: " << endl;
+    // cin >> n;
+
+    
+
+    // for (int i = 1; i <= n; i++) // for lines
+    // {
+
+    //     for (int j = 1; j <= i; j++) //for print
+    // {
+        
+    //     cout << j << " ";
+        
+    // }
+    // cout  << endl;
+    // }
+
+// Q10
+
+    int n;
+
+    cout << "Enter value: " << endl;
+    cin >> n;
+
+    
+
+    for (int i = 0; i < n; i++) // for lines
     {
-        for (char j = 'A'; j <= n; j++) //for print
+        int var = 0;
+        
+        for (int j = i+1; j <= j-var; j++) //for print
     {
+        
         cout << j << " ";
+        var++;
+        
     }
     cout  << endl;
     }
+
     return 0;
+
+
 }
