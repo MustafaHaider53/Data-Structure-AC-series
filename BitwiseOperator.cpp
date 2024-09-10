14355efee7f58e7e080937533a503d1fe72a69c3
Mustafa53 Haider <mustafahaider523@gmail.com>
2024-09-10 05:04:53 +0000

    7 video

---
+++ BitwiseOperator.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+
+    // operation a * 2 to the power b == a << b left shift aoperator
+    // operation a / 2 to the power b == a >> b right shift operator
+
+//    cout << (6 & 10) << endl; 
+//    cout << (6 | 10) << endl; 
+//    cout << (6 ^ 10) << endl; 
+
+//    cout << (10>>1) << endl;
+//    cout << (10<<2) << endl;
+
+
+ 
+ 
+ 
+    return 0;
+}
