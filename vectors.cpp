5c35cc22815d6a1fbaf606a987ebe762b4d9c756
Mustafa53 Haider <mustafahaider523@gmail.com>
2024-09-14 05:48:22 +0000

    lecture 8

---
+++ vectors.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+ 
+int main()
+{
+    vector<int> vec = {1,2,3,4,5 ,6};
+
+    // vectors functions
+    // size
+    // push_back
+    // pop_back
+    // front
+    // at
+
+    // cout << "size = " << vec.size() << endl;
+
+    // vec.push_back(25);
+
+    // cout << "size = " << vec.size() << endl;
+
+    // vec.pop_back();
+
+    // cout << "size = " << vec.size() << endl;
+
+    // cout << vec.front() << endl; 
+
+    // cout << vec.at(3) << endl;
+
+    //Q1 iterate throug a vector using for loop
+    // for (int i = 0; i < vec.size(); i++)
+    // {
+    //     cout << vec.at(i) << " ";
+    // }
+
+    cout  << endl;
+
+    // Q2 reverse a vector
+
+    // int var = 0;
+    // vector<int> result; 
+
+    
+
+    
+
+
+    //   for (int i = 1; i <= vec.size(); i++)
+    //   {
+    //          var = vec.at(vec.size()- i);
+    //             result.push_back(var);
+                
+    //   }
+      
+
+    // for (int i = 0; i < vec.size(); i++)
+    // {
+    //     cout << result.at(i) << " ";
+    // }
+    
+    
+
+
+    
+ 
+ 
+ 
+    return 0;
+}
